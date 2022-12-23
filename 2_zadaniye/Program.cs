812fb3f1daec7c90286c9362efec793df51ad8c0
Alina <alina-berdnikova@inbox.ru>
2022-12-23 19:39:57 +0300

    4 zadan

---
+++ 2_zadaniye/Program.cs
@@ -0,0 +1,14 @@
+﻿{
+            Console.WriteLine("Программа нахождения максимального из двух.");
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a < b) Console.WriteLine($"Число {b} большее.");
+            else Console.WriteLine($"Число {a} большее.");
+
+            Console.WriteLine("Для продолжения нажмите любую клавишу...");
+            Console.ReadKey();
+        }
---
+++ 8_zadaniye/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+            Console.WriteLine("Программа выдачи ряда четных чисел до значения.");
+            Console.Write("Введите число: ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            for (int counter = 0; counter <= input; counter++)
+            {
+                if (counter % 2 == 0)
+                {
+                    Console.Write(counter + " ");
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Выполнение программы завершено.");
+        }
